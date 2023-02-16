4460955011105b8f0c201b3bcadf99257f7a94c9
Nichita <nichita.litvinov@productmadness.com>
2023-02-16 11:49:45 +0100

    create framework

---
+++ Migratin/Migratin/Migratin.h
@@ -0,0 +1,18 @@
+//
+//  Migratin.h
+//  Migratin
+//
+//  Created by Nichita Litvinov on 16/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Migratin.
+FOUNDATION_EXPORT double MigratinVersionNumber;
+
+//! Project version string for Migratin.
+FOUNDATION_EXPORT const unsigned char MigratinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Migratin/PublicHeader.h>
+
+
